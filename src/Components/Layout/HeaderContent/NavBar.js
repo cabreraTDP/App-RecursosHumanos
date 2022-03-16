99a09a5b0b9a82666a4a7b7489c4f9f422716834
GuillermoTDP <guillermomartinez@techdatapeople.com>
2022-03-16 12:12:35 -0600

    barra de navegación

--- src/Components/Layout/HeaderContent/NavBar.js
+++ src/Components/Layout/HeaderContent/NavBar.js
@@ -1,7 +1,20 @@
+import '../../../CSS/NavBar.css'
+import heb from './/heb.png'
 const NavBar = () => {
     return (
-        <div className="inline">
-            NavBar
+        <div className="inline" style={{}}>
+            <div >
+                <img src={heb} alt="BigCo Inc. logo" style={{ width: '300px', height:'80px' }} />
+            </div>
+            <div id='opcion-select'>
+                CANDIDATOS EN PROCESO
+            </div>
+            <div id='opcion'>
+                CANDIDATOS CONTRATADOS
+            </div>
+            <div id='opcion' style={{ lineHeight: '70px' }}>
+                VACANTES
+            </div>
         </div>
     );
 }
